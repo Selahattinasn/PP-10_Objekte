db1a8198a198baf23706db8772c653ada1d507e8
Peter <schmidt.multimedia@gmail.com>
2021-11-26 12:29:49 +0100

    1. Objekt / properties

--- script_01.js
+++ script_01.js
@@ -9,10 +9,35 @@
 // output(arr[0][2]);
 // output(arr[1][2]);
 
-
 /***** Objekte 1 Daten/Funktionen *******/
 
+const person = 
+{       
+        firstName: "Patrick",
+        familyName: "Polgar",
+        salary:[120000,160000],
+        permission: true,
+        sayHello: 
+                function(){
+                        return "Hi, ich bin " + this.firstName;
+                }
 
+};
+
+output(person);
+output(person.firstName);
+person.firstName = "Patrik";
+output(person.familyName);
+output(person.salary[1]);
+output(person.permission);
+output(person.sayHello());
+// output(person.firstName);
+
+const txt =     "Ich bin " + person.firstName + " " +
+                person.familyName + " und verdiene " +
+                person.salary[1] + " p.a."
+
+output(txt);
 
 
 /** Ausgabe */
